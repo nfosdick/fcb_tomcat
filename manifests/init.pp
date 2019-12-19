ff5c187ab2b20a13abcb71ae8ed6c406cc00a619
Nick Fosdick <nick@lark-it.com>
2019-12-18 20:13:11 -0700

    update

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,6 @@ class fcb_tomcat {
   class { 'java': }
 
   tomcat::install { '/opt/tomcat9':
-    source_url => 'https://www.apache.org/dist/tomcat/tomcat-9/v9.0.x/bin/apache-tomcat-9.0.x.tar.gz'
+    source_url => 'https://www.apache.org/dist/tomcat/tomcat-9/v9.0.30/bin/apache-tomcat-9.0.30.tar.gz',
   }
 }
