48367532e8fd2b7ce1d515fa4c05c80a39448033
Nick Fosdick <nick@lark-it.com>
2019-12-18 21:35:04 -0700

    updates

--- manifests/linux.pp
+++ manifests/linux.pp
@@ -1,5 +1,6 @@
 class fcb_tomcat::linux(
   $install_versions,
+  $instances, 
   #https://archive.apache.org/dist/tomcat/tomcat-8/v${tomcat_8_5_version}/bin/apache-tomcat-${tomcat_8_5_version}.tar.gz"
 ){
   $install_versions.each |$install_dir, $hash| {
@@ -8,7 +9,7 @@ class fcb_tomcat::linux(
     }
   }
 
-  $install_versions.each |$instance, $hash| {
+  $instances.each |$instance, $hash| {
     tomcat::instance { $instance:
       * => $hash,
     }
