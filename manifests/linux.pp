535d6281e23729eb4bfd32b4b3f4418f37f17bfc
Nick Fosdick <nick@lark-it.com>
2019-12-18 20:50:17 -0700

    update

--- manifests/linux.pp
+++ manifests/linux.pp
@@ -1,6 +1,17 @@
-class fcb_tomcat::linux {
+class fcb_tomcat::linux(
+  $install,
+  #$install_dir = '/opt/tomcat-9',
+  #$install_url = 'https://www.apache.org/dist/tomcat/tomcat-9/v9.0.30/bin/apache-tomcat-9.0.30.tar.gz',
+  #https://archive.apache.org/dist/tomcat/tomcat-8/v${tomcat_8_5_version}/bin/apache-tomcat-${tomcat_8_5_version}.tar.gz"
+){
 
-  tomcat::install { '/opt/tomcat9':
-    source_url => 'https://www.apache.org/dist/tomcat/tomcat-9/v9.0.30/bin/apache-tomcat-9.0.30.tar.gz',
+  $hash.each |$key, $values| {
+    notify{"Nick $values['install_dir'] and $values['install_url]":}
+    #tomcat::install { $values['install_dir']:
+    #  source_url => $values['install_url],
+    #}
   }
+    #tomcat::install { $install_dir:
+    #  source_url => $install_url,
+    #}
 }
