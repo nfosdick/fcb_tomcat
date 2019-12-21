16e6566b0b4bef7aa17c6fa160abead0276f818e
Nick Fosdick <nick@lark-it.com>
2019-12-21 13:40:22 -0700

    testnig

--- manifests/linux.pp
+++ manifests/linux.pp
@@ -28,7 +28,7 @@ class fcb_tomcat::linux(
   $connectors.each |$instance, $connector_hash| {
     $catalina_base = $instances[$instance]['catalina_base']
     $defaults = $defaults_connectors['defaults']
-    notify{"Nick $default":}
+    notify{"Nick $defaults":}
     #$merged_connectors = $defaults_connectors['defaults'] + $connector_hash
     $connector_hash.each |$connector, $hash| { 
       tomcat::config::server::connector { "${instance}-${connector}":
