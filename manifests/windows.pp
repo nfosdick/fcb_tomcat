2da14e4ca19a2365cd8774e3e26dcd8a8586fe51
Nick Fosdick <nick@lark-it.com>
2019-12-22 21:23:24 -0700

    Updating windows

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -1,3 +1,14 @@
-class fcb_tomcat::windows {
+class fcb_tomcat::windows(
+  $destination_path = 'c:/larktemp',
+  $version          = '9.0.30',
+  $architecture     = $facts['architecture'],
+  $zip_file         = "apache-tomcat-${version}-windows-${architecture}.zip",
+  $download_uri     = "https://www-eu.apache.org/dist/tomcat/tomcat-9/v${version}/bin",
+  $download_url     = "${download_uri}/${zip_file}",
+){
 
+  dsc_xremotefile {"Download $url":
+    dsc_destinationpath => "${destination_path}/${zip_file}",
+    dsc_uri             => $url,
+  }
 }
