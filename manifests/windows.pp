0f54ce4e43055d42f2f29b69803a2736e5dba00d
Nick Fosdick <nick@lark-it.com>
2019-12-28 21:01:22 -0700

    testing

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -31,7 +31,7 @@ class fcb_tomcat::windows(
 #  unless   => "if(Get-Command java | Select-Object Version|Select-String ${java_version}){ exit 0 }else{ exit 1 }",
   exec { "Install tomcat-${version} Windows Service":
     command   => "$service_cmd install tomcat-${version}",
-    onlyif    => "Get-Command tomcat-${version}",
+    onlyif    => "if(Get-Command tomcat-${version}){ exit 0 }else{ exit 1 }",
     provider  => powershell,
     #logoutput => true,
     require   => Dsc_archive[ "Unzip $zip_file" ],
