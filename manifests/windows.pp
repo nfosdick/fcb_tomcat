2c2f71355bc6e1e6c03b7ca0999c53ff59381ab5
Nick Fosdick <nick@lark-it.com>
2020-08-26 17:11:26 -0600

    Adding update to service name

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -37,7 +37,8 @@ class fcb_tomcat::windows(
   }
 
   dsc_service{"tomcat-${version}":
-    dsc_name  => "${service_name} install tomcat-${version}",
+    #dsc_name  => "${service_name} tomcat-${version}",
+    dsc_name  => "Apache Tomcat 9.0 tomcat-9.0.37",
     dsc_state => 'running',
     require   => Exec[ "Install tomcat-${version} Windows Service" ],
   }  
