91849705ac06de8be899b7ebd214b3b9cb24e024
Nick Fosdick <nick@lark-it.com>
2020-08-27 17:17:21 -0600

    Setting up concat fragments

--- manifests/windows/config.pp
+++ manifests/windows/config.pp
@@ -4,4 +4,10 @@ class fcb_tomcat::windows::config{
   concat { "${base_path}/conf/server.xml":
     ensure => present,
   }
+
+  concat::fragment { 'httpd.conf':
+    target  => "${base_path}/conf/server.xml",
+    content => template("${module_name}/begin_server_xml.erb"),
+    order   => '01',
+  }
 }
