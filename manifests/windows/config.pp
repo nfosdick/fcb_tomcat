5b32eccfc22c33d8ffba86517a469221d1b63493
Nick Fosdick <nick@lark-it.com>
2020-08-26 20:11:56 -0600

    Adding config

--- manifests/windows/config.pp
+++ manifests/windows/config.pp
@@ -1,7 +1,7 @@
 class fcb_tomcat::windows::config{
   $base_path = $::fcb_tomcat::windows::base_path
 
-  concat { "${base_path}/config/server.xml":
+  concat { "${base_path}/conf/server.xml":
     ensure => present,
   }
 }
