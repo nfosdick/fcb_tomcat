dadceabc3bc65ab8c37e06c67a4a807e0279d95a
Nick Fosdick <nick@lark-it.com>
2020-08-27 17:26:01 -0600

    Adding end

--- manifests/windows/config.pp
+++ manifests/windows/config.pp
@@ -5,9 +5,15 @@ class fcb_tomcat::windows::config{
     ensure => present,
   }
 
-  concat::fragment { 'httpd.conf':
+  concat::fragment { 'Start server.xml':
     target  => "${base_path}/conf/server.xml",
     content => template("${module_name}/begin_server_xml.erb"),
     order   => '01',
+  }
+
+  concat::fragment { 'End server.xml':
+    target  => "${base_path}/conf/server.xml",
+    content => template("${module_name}/end_server_xml.erb"),
+    order   => '99',
   }
 }
