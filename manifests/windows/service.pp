0103c4e5c984893e3260ef8f3545580a139a86d7
Nick Fosdick <nick@lark-it.com>
2020-08-29 16:30:19 -0600

    Adding service file

--- manifests/windows/service.pp
+++ manifests/windows/service.pp
@@ -1,11 +1,11 @@
 class fcb_tomcat::windows::service
 {
   $service_name = $fcb_tomcat::windows::service_name
-  $version      = $fcb_tomcat::windows::service_name
+  $version      = $fcb_tomcat::windows::version
 
   dsc_service{ "${service_name} tomcat-${version}":
     dsc_name  => "${service_name} tomcat-${version}",
     dsc_state => 'running',
- #   require   => Exec[ "Install tomcat-${version} Windows Service" ],
+    require   => Exec[ "Install tomcat-${version} Windows Service" ],
   }  
 }
