9753f796a3a0a70d31c6413e0a0eea94fb2cc872
Nick Fosdick <nick@lark-it.com>
2020-08-29 17:21:02 -0600

    Major refactor

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -8,8 +8,7 @@ class fcb_tomcat::windows(
   $zip_file         = "apache-tomcat-${version}-windows-${architecture}.zip",
   $download_uri     = "http://www-eu.apache.org/dist/tomcat/tomcat-9/v${version}/bin",
 ){
-  $base_path     = "${install_dir}/apache-tomcat-${version}"
-  $download_url  = "${download_uri}/${zip_file}"
+  $base_path = "${install_dir}/apache-tomcat-${version}"
 
   include fcb_tomcat::windows::install
 
@@ -20,30 +19,4 @@ class fcb_tomcat::windows(
   class{ 'fcb_tomcat::windows::service':
     require => Class[ 'fcb_tomcat::windows::config' ]
   }
-
-#  dsc_xremotefile {"Download $download_url":
-#    dsc_destinationpath => $zip_file_path,
-#    dsc_uri             => $download_url,
-#    before              => Dsc_archive[ "Unzip $zip_file" ],
-#  }
-
-#  file { $install_dir:
-#    ensure => directory,
-#    before => Dsc_archive[ "Unzip $zip_file" ],
-#  }
-
-#  dsc_archive {"Unzip $zip_file":
-#    dsc_ensure      => 'present',
-#    dsc_path        => $zip_file_path,
-#    dsc_destination => $install_dir,
-#  }
-
-#  exec { "Install tomcat-${version} Windows Service":
-#    command   => "$service_cmd install tomcat-${version}",
-#    unless    => "if(Get-Service tomcat-${version}){ exit 0 }else{ exit 1 }",
-#    provider  => powershell,
-#    logoutput => true,
-#    cwd       => "${base_path}/bin",
-#    require   => Dsc_archive[ "Unzip $zip_file" ],
-#  }
 }
